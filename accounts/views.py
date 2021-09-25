232139cbb1be9d1a2f9c99bf252189904daa9a3c
Sihan Tawsik <sihantawsik@gmail.com>
2021-09-25 18:57:56 +0600

    Add profile list

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+
+from pdf.serializers import PdfSerializer
+from pdf.models import PdfFile
 
 # Create your views here.
+
+
+class ProfilePdfList(ListAPIView):
+    serializer_class = PdfSerializer
+
+    def get_queryset(self):
+        return PdfFile.objects.filter(user=self.request.user)
